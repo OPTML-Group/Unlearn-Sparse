3e6807bc2caab4eb7cfd6e8d8d37ec4862ee0b99
ljcc <ljcc0930@qq.com>
2022-10-18 20:26:14 +0800

    update unlearn method

--- unlearn/GA.py
+++ unlearn/GA.py
@@ -1,5 +1,8 @@
 import time
+import matplotlib.pyplot as plt
+import os
 
+from trainer import validate
 import utils
 
 def GA_iter(train_loader, model, criterion, optimizer, epoch, args):
@@ -47,3 +50,53 @@ def GA_iter(train_loader, model, criterion, optimizer, epoch, args):
     print('train_accuracy {top1.avg:.3f}'.format(top1=top1))
 
     return top1.avg
+
+def GA(forget_loader, retain_loader, test_loader, val_loader, model, criterion, optimizer, scheduler, args):
+    all_result = {}
+    all_result['retain_ta'] = []
+    all_result['test_ta'] = []
+    all_result['val_ta'] = []
+    all_result['forget_ta'] = []
+    start_epoch = 0
+    start_state = 0
+    for epoch in range(0, args.epochs):
+        start_time = time.time()
+        print(optimizer.state_dict()['param_groups'][0]['lr'])
+        acc = GA_iter(forget_loader, model, criterion, optimizer, epoch, args)
+
+        # evaluate on validation set
+        tacc = validate(val_loader, model, criterion, args)
+        # evaluate on test set
+        test_tacc = validate(test_loader, model, criterion, args)
+        # evaluate on retain set
+        f_tacc = validate(retain_loader, model, criterion, args)
+        scheduler.step()
+
+        all_result['retain_ta'].append(f_tacc)
+        all_result['test_ta'].append(tacc)
+        all_result['val_ta'].append(test_tacc)
+        all_result['forget_ta'].append(acc)
+        # remember best prec@1 and save checkpoint
+        is_best_sa = tacc  > best_sa
+        best_sa = max(tacc, best_sa)
+
+        utils.save_checkpoint({
+            'state': 0,
+            'result': all_result,
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'best_sa': best_sa,
+            'optimizer': optimizer.state_dict(),
+            'scheduler': scheduler.state_dict(),
+            'init_weight': None
+        }, is_SA_best=is_best_sa, pruning=0, save_path=args.save_dir)
+
+        # plot training curve
+        plt.plot(all_result['retain'], label='train_acc')
+        plt.plot(all_result['val_ta'], label='val_acc')
+        plt.plot(all_result['test_ta'], label='test_acc')
+        plt.plot(all_result['forget_ta'], label='forget_acc')
+        plt.legend()
+        plt.savefig(os.path.join(args.save_dir, str(0)+'net_train.png'))
+        plt.close()
+        print("one epoch duration:{}".format(time.time()-start_time))
--- unlearn/__init__.py
+++ unlearn/__init__.py
@@ -1,10 +1,12 @@
 import GA
 import RL
 
-def get_unlearn_method_iter(name):
+def get_unlearn_method(name):
+    # method usage: (TODO)
+    #   function(forget_loader, retain_loader, test_loader, val_loader, model, criterion, optimizer, scheduler, args)
     if name == "RL":
-        return RL.RL_iter
+        return RL.RL
     elif name == "GA":
-        return GA.GA_iter
+        return GA.GA
     else:
         raise NotImplementedError(f"Unlearn method {name} not implemented!")
