e7bf583194af7b2965040853cae0cb7d4a008616
JC Liu <ljcc0930@qq.com>
2022-10-20 14:10:52 -0400

    add original

--- unlearn/__init__.py
+++ unlearn/__init__.py
@@ -4,11 +4,16 @@ from .FT import FT
 from .fisher import fisher
 from .retrain import retrain
 
+def raw(data_loaders, model, criterion, args):
+    pass
+
 def get_unlearn_method(name):
     """ method usage:
     
     function(data_loaders, model, criterion, args)"""
-    if name == "RL":
+    if name == "raw":
+        return raw
+    elif name == "RL":
         return RL
     elif name == "GA":
         return GA
