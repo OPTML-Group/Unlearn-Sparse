42d19d811fe87e8cd9f54b6117836a5b4e130e01
JC Liu <ljcc0930@qq.com>
2022-10-27 12:12:14 -0400

    Add param for run.py

--- run.py
+++ run.py
@@ -1,27 +1,41 @@
 from utils import run_commands
 
+params = {
+    "FT": "--epoch 10 --lr 0.01",
+    "GA": "--epoch 10 --lr 0.01",
+    "RL": "--epoch 10 --lr 0.01"
+}
 
-def gen_commands():
+
+def gen_commands(rerun = False):
     commands = []
     sparsities = "0p5 0p75 0p9 0p99 0p95 0p995".split(' ')
-    methods = "raw fisher retrain FT RL".split(' ')  # GA
-    nums = [100, 4500, 2250, 450]
-    seeds = [1, 4, 2]
+    methods = "FT GA RL".split(' ')  # GA RL  raw fisher retrain
+    nums = [100, 4500]# , 2250, 450]
+    seeds = [1, 2, 4]
     for seed in seeds:
         for num in nums:
             for unlearn in methods:
-                command = f"python -u main_forget.py --save_dir unlearn_results/dense/{unlearn}_{num}/seed{seed} --mask pruning_models/dense/seed1/state_dict.pth --unlearn {unlearn} --num_indexes_to_replace {num} --seed {seed} --resume"
+                command = f"python -u main_forget.py --save_dir unlearn_results/dense/{unlearn}_{num}/seed{seed} --mask pruning_models/dense/seed1/state_dict.pth --unlearn {unlearn} --num_indexes_to_replace {num} --seed {seed}"
+                if unlearn in params:
+                    command = command + ' ' + params[unlearn]
+                if not rerun:
+                    command = command + ' --resume'
                 commands.append(command)
         for sparsity in sparsities:
             for num in nums:
                 for unlearn in methods:
-                    command = f"python -u main_forget.py --save_dir unlearn_results/{sparsity}/{unlearn}_{num}/seed{seed} --mask pruning_models/synflow_iterative/ratio{sparsity}/seed1/state_dict.pth --unlearn {unlearn} --num_indexes_to_replace {num} --seed {seed} --resume"
+                    command = f"python -u main_forget.py --save_dir unlearn_results/{sparsity}/{unlearn}_{num}/seed{seed} --mask pruning_models/synflow_iterative/ratio{sparsity}/seed1/state_dict.pth --unlearn {unlearn} --num_indexes_to_replace {num} --seed {seed}"
+                    if unlearn in params:
+                        command = command + ' ' + params[unlearn]
+                    if not rerun:
+                        command = command + ' --resume'
                     commands.append(command)
     return commands
 
 
 if __name__ == "__main__":
-    commands = gen_commands()
+    commands = gen_commands(rerun = True)
     print(len(commands))
-    run_commands(list(range(8)) * 2, commands, call=True,
-                 dir="commands", shuffle=False, delay=0.5)
+    run_commands(list(range(8)), commands, call=True,
+                 dir="commands", shuffle=True, delay=0.5)
