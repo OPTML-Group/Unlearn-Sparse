032b10f582dd032e7386f38c8aeaf939691de699
JC Liu <ljcc0930@qq.com>
2022-12-15 04:41:08 -0500

    upd woodfisher approx

--- unlearn/Wfisher.py
+++ unlearn/Wfisher.py
@@ -1,8 +1,6 @@
-from sre_constants import SUCCESS
 import torch
 from torch.autograd import grad
 from tqdm import tqdm
-import copy
 
 def sam_grad(model,loss):
     params = []
@@ -11,8 +9,8 @@ def sam_grad(model,loss):
     sample_grad = grad(loss,params)
     sample_grad = [x.view(-1) for x in sample_grad]
     return torch.cat(sample_grad)
+
 def apply_perturb(model,v):
-    params = []
     curr = 0 
     for param in model.parameters():
         length = param.view(-1).shape[0]
@@ -24,7 +22,7 @@ def apply_perturb(model,v):
         
 def woodfisher(model,train_dl,device,criterion,v):
     model.eval()
-    k_new = v
+    k_vec = torch.clone(v)
     N = len(train_dl)
     
     for idx,(data,label) in enumerate(tqdm(train_dl)):
@@ -32,22 +30,20 @@ def woodfisher(model,train_dl,device,criterion,v):
         label = label.to(device)
         output = model(data)
         loss = criterion(output,label)
-        sample_grad = sam_grad(model,loss)
+        sample_grad = sam_grad(model,loss)  
         if idx == 0:
-            o_new = sample_grad
+            o_vec = torch.clone(sample_grad)
         else:
-            o_old = o_new
-            k_old = k_new
-            tmp = torch.matmul(o_old,sample_grad)
-            o_new = o_old - (tmp/(N+tmp))*o_old
-            k_new = k_old - (tmp/(N+tmp))*k_old
-    return k_new
+            tmp = torch.dot(o_vec, sample_grad)
+            k_vec -= (torch.dot(k_vec, sample_grad) / (N + tmp)) * o_vec
+            o_vec -= (tmp / (N + tmp)) * o_vec
+    return k_vec
 
 def Wfisher(data_loaders,model,criterion,args):
     retain_loader = data_loaders["retain"]
     forget_loader = data_loaders["forget"]
-    retain_loader = torch.utils.data.DataLoader(retain_loader.dataset, batch_size=1, shuffle=False)
-    forget_loader = torch.utils.data.DataLoader(forget_loader.dataset, batch_size=1, shuffle=False)
+    retain_loader = torch.utils.data.DataLoader(retain_loader.dataset, batch_size=32, shuffle=False)
+    forget_loader = torch.utils.data.DataLoader(forget_loader.dataset, batch_size=128, shuffle=False)
     device = f"cuda:{int(args.gpu)}" if torch.cuda.is_available() else "cpu"
     params = []
     for param in model.parameters():
@@ -67,7 +63,7 @@ def Wfisher(data_loaders,model,criterion,args):
 
     perturb = woodfisher(model,retain_loader,device=device,criterion=criterion,v=forget_grad)
     
-    apply_perturb(model,perturb)
+    apply_perturb(model, args.alpha * perturb)
 
     return model
 
@@ -85,7 +81,7 @@ def woodfisher_batch(model,inputs,labels,device,criterion,v):
         label = labels[i]
         output = model(data)
         loss = criterion(output,label)
-        sample_grad =  grad(loss,params)
+        sample_grad = grad(loss,params)
         if i == 0:
             o_new = sample_grad
         else:
