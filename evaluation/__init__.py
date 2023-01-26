ddec2037183853286d1be18ecd1bbd7f85267845
JC Liu <ljcc0930@qq.com>
2023-01-25 22:02:34 -0500

    remove files

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -1,5 +1,3 @@
 from .MIA import MIA
-from .efficacy import efficacy, efficacy_upper_bound
 from .SVC_MIA import SVC_MIA
 from .verification_error import verification_error
-from .lanczos import lanczos
