17390be031602d615792335bba26bacde1ad16f6
JC Liu <ljcc0930@gmail.com>
2026-02-27 02:45:05 -0800

    unify sam train

--- pruning_pipeline.py
+++ pruning_pipeline.py
@@ -17,6 +17,7 @@ from pruner import (
     remove_prune,
 )
 from trainer import train as default_train
+from trainer import train_sam_epoch
 from trainer import validate
 
 
@@ -65,54 +66,6 @@ def _build_optimizer(args, model, *, mode):
     )
 
 
-def _sam_train(train_loader, model, criterion, optimizer, epoch, args):
-    losses = utils.AverageMeter()
-    top1 = utils.AverageMeter()
-    model.train()
-    start = time.time()
-
-    for i, (image, target) in enumerate(train_loader):
-        if epoch < args.warmup:
-            utils.warmup_lr(
-                epoch, i + 1, optimizer, one_epoch_step=len(train_loader), args=args
-            )
-
-        image = image.cuda()
-        target = target.cuda()
-
-        output_clean = model(image)
-        loss = criterion(output_clean, target)
-        loss.backward()
-        optimizer.first_step(zero_grad=True)
-
-        output_clean = model(image)
-        loss = criterion(output_clean, target)
-        loss.backward()
-        optimizer.second_step(zero_grad=True)
-
-        output = output_clean.float()
-        loss = loss.float()
-        prec1 = utils.accuracy(output.data, target)[0]
-
-        losses.update(loss.item(), image.size(0))
-        top1.update(prec1.item(), image.size(0))
-
-        if (i + 1) % args.print_freq == 0:
-            end = time.time()
-            print(
-                "Epoch: [{0}][{1}/{2}]\t"
-                "Loss {loss.val:.4f} ({loss.avg:.4f})\t"
-                "Accuracy {top1.val:.3f} ({top1.avg:.3f})\t"
-                "Time {3:.2f}".format(
-                    epoch, i, len(train_loader), end - start, loss=losses, top1=top1
-                )
-            )
-            start = time.time()
-
-    print("train_accuracy {top1.avg:.3f}".format(top1=top1))
-    return top1.avg
-
-
 def _get_profile(profile_name):
     if profile_name == "imp":
         return {
@@ -131,7 +84,7 @@ def _get_profile(profile_name):
     if profile_name == "sam":
         return {
             "criterion": torch.nn.CrossEntropyLoss(),
-            "train_fn": _sam_train,
+            "train_fn": train_sam_epoch,
             "initial_optimizer_mode": "sam_initial",
             "initial_scheduler_mode": "multistep",
         }
--- trainer/train.py
+++ trainer/train.py
@@ -90,6 +90,53 @@ def train(train_loader, model, criterion, optimizer, epoch, args, l1=False):
     return top1.avg
 
 
+def train_sam_epoch(train_loader, model, criterion, optimizer, epoch, args):
+    losses = utils.AverageMeter()
+    top1 = utils.AverageMeter()
+    model.train()
+    start = time.time()
+
+    for i, data in enumerate(train_loader):
+        if epoch < args.warmup:
+            utils.warmup_lr(
+                epoch, i + 1, optimizer, one_epoch_step=len(train_loader), args=args
+            )
+
+        image, target = _get_batch(data, args)
+
+        output_clean = model(image)
+        loss = criterion(output_clean, target)
+        loss.backward()
+        optimizer.first_step(zero_grad=True)
+
+        output_clean = model(image)
+        loss = criterion(output_clean, target)
+        loss.backward()
+        optimizer.second_step(zero_grad=True)
+
+        output = output_clean.float()
+        loss = loss.float()
+        prec1 = utils.accuracy(output.data, target)[0]
+
+        losses.update(loss.item(), image.size(0))
+        top1.update(prec1.item(), image.size(0))
+
+        if (i + 1) % args.print_freq == 0:
+            end = time.time()
+            print(
+                "Epoch: [{0}][{1}/{2}]\t"
+                "Loss {loss.val:.4f} ({loss.avg:.4f})\t"
+                "Accuracy {top1.val:.3f} ({top1.avg:.3f})\t"
+                "Time {3:.2f}".format(
+                    epoch, i, len(train_loader), end - start, loss=losses, top1=top1
+                )
+            )
+            start = time.time()
+
+    print("train_accuracy {top1.avg:.3f}".format(top1=top1))
+    return top1.avg
+
+
 def train_with_rewind(model, optimizer, scheduler, train_loader, criterion, args):
     rewind_state_dict = None
     for epoch in range(args.epochs):
